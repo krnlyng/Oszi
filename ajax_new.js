056c34c60138bbaddc959f54eb8beb0f95e3d066
Daniel Gröber <darklord@darkboxed.org>
2010-06-05 16:26:02 +0200

    Initial

---
+++ ajax_new.js
@@ -0,0 +1,42 @@
+var ajaxBusy=false;
+function ajax(a) {
+    if(ajaxBusy) {
+        return;
+    }
+    ajaxBusy=true;
+    var d;
+    try {
+        d=new XMLHttpRequest();
+    } catch(e) {
+        d=new ActiveXObject('Microsoft.XMLHTTP')
+    }
+    var f=function() {
+        if(d.readyState==4) {
+            var i=0,v=0,t='0px',l='0px';
+            var str = d.responseText;
+	    var content_all = str.split(";");
+            for(i=0;i<content_all.length;i++) {
+                v=parseInt(content_all[i])+127;
+                t=(v)+'px';
+                l=(i)+'px';
+                if(!document.getElementById(i)) {
+                    var n=document.createElement('div');
+                    document.getElementById('graf').appendChild(n);
+                    n.id=i;
+                    n.style.top=t;
+                    n.style.left=l;
+					n.className = "dot";
+                } else {
+                    document.getElementById(i).style.top=t;
+                    document.getElementById(i).style.left=l;
+                }
+            }
+            ajaxBusy=false;
+            setTimeout('ajax(\''+a+'\')',0);
+        }
+    }
+    d.open('GET',a,true);
+    d.onreadystatechange=f;
+    d.send(null);
+}
+
